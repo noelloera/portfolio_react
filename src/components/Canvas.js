739beeb0065d791045d0dea8012617dfe42d922b
Noel Loera <50557498+noelloera@users.noreply.github.com>
2021-03-09 14:32:06 -0700

    Began Home component to render upon page load
    -Added the Home component
    -Added the Canvas component to render an interactive background
    -Added Navigation component which will hold the page anchors

---
+++ src/components/Canvas.js
@@ -0,0 +1,8 @@
+import "../helpers/canvas.scss";
+export default function Canvas() {
+  return (
+    <div>
+      <canvas id="stars" width="300" height="300"></canvas>
+    </div>
+  );
+}
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import react from "react";
+import Canvas from "./Canvas.js";
+export default function Home() {
+  return (
+    <div>
+      <Canvas />
+    </div>
+  );
+}
