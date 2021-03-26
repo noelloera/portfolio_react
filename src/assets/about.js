5c208adf27d500b42271ec6909fb36dc2c573277
Noel Loera <50557498+noelloera@users.noreply.github.com>
2021-03-25 20:34:46 -0700

    Added the about.js file
    -about.js contains an array holding the information for each of the elements to be rendered
    -added the respective icons for the elements

---
+++ src/assets/about.js
@@ -0,0 +1,26 @@
+const aboutDescriptions = [
+  {
+    title: "Responsive",
+    icon: "src/assets/images/responsive.png",
+    description:
+      "fast request and response load times, seamless desktop and mobile interfaces",
+  },
+  {
+    title: "Dynamic",
+    icon: "src/assets/images/dynamic.png",
+    description:
+      "products and solutions which adapt to changes, needed for optimum results",
+  },
+  {
+    title: "Functional",
+    icon: "src/assets/images/functional.png",
+    description:
+      "Integration between views and logic which ensures client expectations ",
+  },
+  {
+    title: "Efficient",
+    icon: "src/assets/images/efficient.png",
+    description:
+      "Delivery of products and solutions, with an emphasis of stability in mind",
+  },
+];
