cb93777cf0fe532cd0d4d41c69bdcf9c5e2d7093
Noel Loera <50557498+noelloera@users.noreply.github.com>
2021-04-05 21:22:00 -0700

    Added Porfolio Section Component

---
+++ src/components/Portfolio.js
@@ -0,0 +1,14 @@
+import { Typography } from "@material-ui/core";
+import Fade from "react-reveal/Fade";
+
+export default function Portfolio() {
+  return (
+    <section>
+      <header>
+        <Fade left>
+          <Typography variant="subtitle1">PORTFOLIO</Typography>
+        </Fade>
+      </header>
+    </section>
+  );
+}
